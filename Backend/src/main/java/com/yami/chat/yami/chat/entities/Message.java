f0d9c78cbd6ba9d68b7dc68f3f5b5e34e0b87e2c
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2026-02-01 00:03:17 +0530

    Initialize backend structure with Maven setup and basic application files

---
+++ Backend/src/main/java/com/yami/chat/yami/chat/entities/Message.java
@@ -0,0 +1,28 @@
+package com.yami.chat.yami.chat.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    private String sender;
+    private  String content;
+    private LocalDateTime timeStamp;
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timeStamp = LocalDateTime.now();
+    }
+}
